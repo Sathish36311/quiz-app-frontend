48e3af15821efa2cf9e119f7a0b83d2cc8423f7f
Sathish Krishnan <150322266+Sathish36311@users.noreply.github.com>
2025-08-25 09:38:48 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     appDir: true, // ensures Next.js knows to use the app router
-//   },
-// }
-
-// module.exports = nextConfig
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
 
-module.exports = nextConfig;
+}
+
+export default nextConfig;
